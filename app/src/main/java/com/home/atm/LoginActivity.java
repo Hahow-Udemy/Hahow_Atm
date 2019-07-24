c27e5fcafde7b9468b53e44d74ea1f20d15cafbf
pighaddt <pighaddt@gmail.com>
2019-07-24 16:33:23 +0800

    GridView ImageView contact

--- app/src/main/java/com/home/atm/LoginActivity.java
+++ app/src/main/java/com/home/atm/LoginActivity.java
@@ -1,8 +1,14 @@
 package com.home.atm;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +26,7 @@ import com.google.firebase.database.ValueEventListener;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = LoginActivity.class.getSimpleName() ;
+    private static final int REQUEST_CODE = 5 ;
     private EditText edUserid;
     private EditText edPasswd;
     private CheckBox cbRemember;
@@ -28,6 +35,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        int permission =  ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if(permission == PackageManager.PERMISSION_GRANTED){
+//            takePhoto();
+        }else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);
+        }
+
         Log.d(TAG, "onCreate:" + edUserid);
         edUserid = findViewById(R.id.userid);
         edPasswd = findViewById(R.id.passwd);
@@ -47,6 +61,23 @@ public class LoginActivity extends AppCompatActivity {
                 .getString("userid", "");
         edUserid.setText(userid);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                takePhoto();
+            }
+        }
+    }
+
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(intent);
+    }
+
     public void login(View view){
         final String userid = edUserid.getText().toString();
         final String passwd = edPasswd.getText().toString();
